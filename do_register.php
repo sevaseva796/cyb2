64d7d43cffc7a3c2251dfe3f27f824e4635fed27
Всеволод <gadenia111@rambler.ru>
2022-02-24 19:00:57 +0300

    comment added

---
+++ do_register.php
@@ -0,0 +1,41 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>do_register</title>
+</head>
+<body>
+<a href ="index1.html">Индекс</a>
+    <?php
+        $user = $_REQUEST['user'];
+        $pwd = $_REQUEST['pwd'];
+        $hash = hash("sha256", $pwd);
+        include("../../params/billing.php");
+      
+        $conn = mysqli_connect($db_server,$db_user,$db_pwd,"billing");
+    
+        $sql = "INSERT INTO users( Login, Pwdhash) VALUES('$user', '$hash')";
+        $query = mysqli_query($conn,$sql);
+        
+        $result = mysqli_fetch_all($query);
+        
+
+        mysqli_close($conn);
+        
+        if(count($result) == 0)
+            echo("Такой логин уже существует!");
+        else {
+            echo "<h1>Регистрация прошла успешно!<h1>";
+            echo "Сейчас вы будете перенаправлены на главную страницу.";
+            $_SESSION["user"] = $user;
+            echo '<meta http-equiv="refresh" content="2; URL=index1.html"> ';
+        }
+     ?>
+</body>
+</html> 
---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Please, input name and password for creating account.</h1>
+    <form method="POST" action="do_register.php">
+        name <br />
+        <input name="user"/> <br />
+        password <br/>
+        <input type="password" name="pwd"/> <br />
+        <button>Done</button>   
+    </form>
+
+</body>
+</html>
